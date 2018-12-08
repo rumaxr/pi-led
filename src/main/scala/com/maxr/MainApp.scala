fb8f5073174c8ecd970dc43fcc89fcfacafd64e6
Max R <max.remkes@gmail.com>
2018-12-08 16:45:22 +0100

    Added setPinState

--- src/main/scala/com/maxr/MainApp.scala
+++ src/main/scala/com/maxr/MainApp.scala
@@ -6,13 +6,20 @@ object MainApp extends App {
   println("Starting app")
 
   val gpio: GpioController = GpioFactory.getInstance
-  val led: GpioPinDigitalOutput = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_05, "MyLed", PinState.LOW)
+  val led5: GpioPinDigitalOutput = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_05, "Green", PinState.LOW)
+  val led6: GpioPinDigitalOutput = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_06, "Red", PinState.LOW)
 
-  println("Setting pin 5 to high")
-  led.setState(PinState.HIGH)
-  println("Sleeping 5 seconds")
-  Thread.sleep(5000)
-  println("Setting pin 5 to low")
-  led.setState(PinState.LOW)
+  setPinState(led5, PinState.HIGH)
+  setPinState(led5, PinState.LOW)
+  setPinState(led6, PinState.HIGH)
+  setPinState(led6, PinState.LOW)
   println("All done...")
+
+  def setPinState(pin: GpioPinDigitalOutput, state: PinState): Unit = {
+    println(s"Setting pin '{${pin.getName}}' to {${state.getValue}}")
+    pin.setState(state)
+
+    println("Sleeping 2 seconds")
+    Thread.sleep(2000)
+  }
 }
