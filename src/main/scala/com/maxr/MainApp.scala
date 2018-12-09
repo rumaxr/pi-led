aacc588561d147c7b1b56c65dacab9b4218e10a5
Max R <max.remkes@gmail.com>
2018-12-09 17:07:19 +0100

    Added loop

--- src/main/scala/com/maxr/MainApp.scala
+++ src/main/scala/com/maxr/MainApp.scala
@@ -9,13 +9,16 @@ object MainApp extends App {
   val led5: GpioPinDigitalOutput = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_05, "Green", PinState.LOW)
   val led6: GpioPinDigitalOutput = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_06, "Red", PinState.LOW)
 
-  setPinState(led5, PinState.HIGH)
-  setPinState(led5, PinState.LOW, doSleep = true, 500)
-  setPinState(led6, PinState.HIGH)
-  setPinState(led6, PinState.LOW, doSleep = false)
+  0 until 5 foreach { _ =>
+    setPinState(led5, PinState.HIGH)
+    setPinState(led5, PinState.LOW, sleepTime = 100)
+    setPinState(led6, PinState.HIGH)
+    setPinState(led6, PinState.LOW, sleepTime = 100)
+  }
+
   println("All done...")
 
-  def setPinState(pin: GpioPinDigitalOutput, state: PinState, doSleep: Boolean = true, sleepTime: Int = 2000): Unit = {
+  def setPinState(pin: GpioPinDigitalOutput, state: PinState, doSleep: Boolean = true, sleepTime: Int = 500): Unit = {
     println(s"Setting pin '${pin.getName}' to ${state.getValue}")
     pin.setState(state)
 
